3c56e9af1daab45c1b251c7128b9b2b3605c6b35
yj-wong <yujuewong15@gmail.com>
2018-11-22 21:07:24 -0500

    updates

--- cylon.sh
+++ cylon.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 
-counter=0
-while [ $counter -le 3 ]
+#counter0 for mode out
+counter0=0
+while [ $counter0 -le 3 ]
 do
-	gpio mode $counter out
-	((counter++))
+	# gpio mode $counter out
+	((counter0++))
 done
 
+# counter for write 1
+# counter2 for write 0
 counter=0
 while [ 1 ]
 do
-while [ $counter -le 3 ]
-do
-	counter2=0
-	while [ $counter2 -le 3 ]
+	while [ $counter -le 3 ]
 	do
-		gpio write $counter2 0
-		((counter2++))
+
+		counter2=0
+		while [ $counter2 -le 3]
+		do
+			gpio write $counter2 0
+		done
+
+		# echo $counter
+		gpio write $counter 1
+		sleep 0.25
+		((counter++))
+
 	done
 
-	gpio write $counter 1
-	sleep 0.25
+	while [ $counter -gt 0 ]
+	do
 
-	((counter++))
-done
-while
+		counter2=0
+		while [ $counter2 -le 3]
+		do
+			gpio write $counter2 0
+		done
+
+		# echo $counter
+		gpio write $counter 1
+		sleep 0.25
+		((counter--))
+	done
 
 done
